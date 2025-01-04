1f49faae51fe058d0a909de22e3d7adf9bfbf80c
Kenta Ishizeki <a.w.g.d0201@icloud.com>
2025-01-04 18:47:18 +0900

    Remake README

--- test/test.bash
+++ test/test.bash
@@ -6,22 +6,14 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-colcon build  # ビルド
-source $dir/.bashrc  # セットアップ
+colcon build
+source $dir/.bashrc
 
-# ros2 run をバックグラウンドで実行
 timeout 14 ros2 run birthday countdown &
 
-# ノードが立ち上がるまで少し待機
 sleep 3
 
-# トピックを10秒間監視してログに出力
 timeout 11 ros2 topic echo /countdown_topic > /tmp/birthday.log
 
-# 結果をフィルタリング
 grep '誕生日まで残り:' /tmp/birthday.log
-
-# ノードをSIGINTで終了
-#kill -SIGINT $ROS2_PID
-#wait $ROS2_PID  # プロセスの終了を待つ
 
